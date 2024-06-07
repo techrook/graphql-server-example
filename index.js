1954db3118930bda797a69f003b8ab1a0fe17166
techrook <mondayitohowo3@gmail.com>
2024-06-07 12:53:48 +0100

    startup server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // server setup
 
 const server = new ApolloServer({
-
+    
 })
 
 const { url } = await startStandaloneServer(server, {
